d4411fcf787ea175815d492b4d1388a4fa7aee56
jaderaposa <jaduya678@gmail.com>
2023-03-19 15:06:36 +0800

    profile view fixed

--- pages/examples/profile2.php
+++ pages/examples/profile2.php
@@ -280,7 +280,7 @@
                     <div class="btn-group user-helper-dropdown">
                         <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                         <ul class="dropdown-menu pull-right">
-                            <li><a href="javascript:void(0);"><i class="material-icons">person</i>Profile</a></li>
+                            <li><a href="#"><i class="material-icons">person</i>Profile</a></li>
                             <li role="separator" class="divider"></li>
                             <li><a href="javascript:void(0);"><i class="material-icons">group</i>Followers</a></li>
                             <li><a href="javascript:void(0);"><i class="material-icons">shopping_cart</i>Sales</a></li>
@@ -944,7 +944,7 @@
                                             </div>
                                         </div>
                                         <div class="tab-content">
-                                    <div role="tabpanel" class="tab-pane fade in active" id="home">
+
                                         <div class="panel panel-default panel-post">
                                             <div class="panel-heading">
                                                 <div class="media">
